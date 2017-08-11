4ec61a47b2f3f3d9c97909b214be0192c2eef7d2
afanda(范晓宁) <fan_apple1990@163.com>
2017-08-11 20:15:31 +0800

    base project

---
+++ FLKit/FLKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FLKit
+//
+//  Created by afanda on 8/11/17.
+//  Copyright © 2017 easemob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FLKit/FLKit/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FLKit
+//
+//  Created by afanda on 8/11/17.
+//  Copyright © 2017 easemob. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
